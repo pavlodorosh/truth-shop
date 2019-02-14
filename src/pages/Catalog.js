348f52497549dc1c55cd7f428a306d901bd2ca5e
krios22 <krios.1995@yandex.ru>
2019-02-14 19:02:11 +0200

    architecture ver 0.3.1 (product)

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Row, Col, BSpan, Media, BImg, Card } from 'bootstrap-4-react'
+import { Container, Row, Col, Card } from 'bootstrap-4-react'
 import product from '../assets/img/product_img.png'
 class Catalog extends Component {
 	render() {
@@ -8,18 +8,13 @@ class Catalog extends Component {
 			<Container fluid={true}>
 				<Container>
 					<Row className="no-gutter">
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Link to="/product">
 									<Card.Image src={product} />
 								</Link>
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -27,16 +22,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -44,16 +34,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -61,16 +46,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -80,16 +60,11 @@ class Catalog extends Component {
 						</Col>
 					</Row>
 					<Row className="no-gutter">
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -97,16 +72,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -114,16 +84,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -131,16 +96,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -150,16 +110,11 @@ class Catalog extends Component {
 						</Col>
 					</Row>
 					<Row className="no-gutter">
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -167,16 +122,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -184,16 +134,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
@@ -201,16 +146,11 @@ class Catalog extends Component {
 								</Card.Footer>
 							</Card>
 						</Col>
-						<Col className="pl-0 pr-1" col="lg-3">
-							<Card style={{ width: '18rem' }}>
-								<Card.Header>Feature</Card.Header>
+						<Col className="" col="lg-3">
+							<Card>
 								<Card.Image src={product} />
 								<Card.Body>
 									<Card.Title>Card title</Card.Title>
-									<Card.Subtitle mb="2" text="muted">
-										Card subtitle
-									</Card.Subtitle>
-									<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
 								</Card.Body>
 								<Card.Footer>
 									<Card.Link href="#">Card Link</Card.Link>
